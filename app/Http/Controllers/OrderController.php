7aa01417137862f551e589e85c46016be8ac6669
rellisapk <rllskusuma@gmail.com>
2022-05-12 09:57:26 +0700

    first commit

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Treatments;
+use App\Models\User;
+use App\Models\Orders;
+use Illuminate\Support\Facades\DB;
+
+class OrderController extends Controller
+{
+    public function index($id)
+    {
+        $treatments = Treatments::all();
+        $user = User::findOrFail($id);
+        return view('order.order', compact('user', 'treatments'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'treatment' => 'required',
+        ]);
+
+        $order = new Orders;
+        $order->user_id = $request->user()->id;
+        $order->berat = $request->berat;
+        $order->deskripsi = $request->deskripsi;
+        $order->status = $request->status;
+        $order->save();
+
+        $treatment = Treatments::all();
+        foreach($request->treatment as $treatment){
+            $order->treatment_id = $treatment;
+            $servis = Treatments::where('id', $treatment)->first();
+            $totalHarga = $servis->price * $request->berat;
+            $order->total = $totalHarga;
+            $order->save();
+        }
+
+        return back()->with('success', 'Pesanan Baru Dibuat');
+    }
+
+    public function riwayat($id)
+    {
+        $orders = DB::table('orders')
+                    ->join('users','users.id','=','orders.user_id')
+                    ->join('treatments','treatments.id','=','orders.treatment_id')
+                    ->select('orders.*', 'users.name as u_name','treatments.name as t_name')
+                    ->where('orders.user_id', $id)
+                    ->get();
+        return view('order.riwayat',['orders'=>$orders]);
+     
+    }
+}
---
+++ resources/views/order/riwayat.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Riwayat Pemesanan</div>
+                <div class="card-body">
+                <table class="table table-bordered table-primary table-hover text-center">
+                                <thead class="table-primary">
+                                    <tr>
+                                        <th>No</th>
+                                        <!-- <th>ID Order</th> -->
+                                        <th>Tanggal Order</th>
+                                        <th>Berat</th>
+                                        <th>Total Harga</th>
+                                        <th>Deskripsi</th>
+                                        <th>Jenis Treatment</th>
+                                        <th>Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($orders as $o)
+                                <tr>
+                                    <th>{{$loop->iteration}}</th>
+                                    <!-- <th>{{$o->id}}</th> -->
+                                    <th>{{$o->created_at}}</th>
+                                    <th>{{$o->berat}}</th>
+                                    <th>{{$o->total}}</th>
+                                    <th>{{$o->deskripsi}}</th>
+                                    <th>{{$o->t_name}}</th>
+                                    <th>{{$o->status}}</th>
+                                </tr>
+                                @endforeach
+                                    <!-- Masukin syntax sql disini -->
+                                </tbody>
+                            </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
