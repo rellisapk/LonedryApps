6577d217714995f114696f17893b472026b1baf4
Anastasia Victoria <victoriatasia@gmail.com>
2022-05-13 21:00:37 +0700

    riwayat transaksi

--- resources/views/order/riwayat.blade.php
+++ resources/views/order/riwayat.blade.php
@@ -6,35 +6,35 @@
             <div class="card">
                 <div class="card-header">Riwayat Pemesanan</div>
                 <div class="card-body">
-                <table class="table table-bordered table-primary table-hover text-center">
-                                <thead class="table-primary">
-                                    <tr>
-                                        <th>No</th>
-                                        <!-- <th>ID Order</th> -->
-                                        <th>Tanggal Order</th>
-                                        <th>Berat</th>
-                                        <th>Total Harga</th>
-                                        <th>Deskripsi</th>
-                                        <th>Jenis Treatment</th>
-                                        <th>Status</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($orders as $o)
-                                <tr>
-                                    <th>{{$loop->iteration}}</th>
-                                    <!-- <th>{{$o->id}}</th> -->
-                                    <th>{{$o->created_at}}</th>
-                                    <th>{{$o->berat}}</th>
-                                    <th>{{$o->total}}</th>
-                                    <th>{{$o->deskripsi}}</th>
-                                    <th>{{$o->t_name}}</th>
-                                    <th>{{$o->status}}</th>
-                                </tr>
-                                @endforeach
-                                    <!-- Masukin syntax sql disini -->
-                                </tbody>
-                            </table>
+                    <table class="table table-bordered table-hover table-responsive-xl">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th>No</th>
+                                <!-- <th>ID Order</th> -->
+                                <th>Tanggal Order</th>
+                                <th>Berat</th>
+                                <th>Total Harga</th>
+                                <th>Deskripsi</th>
+                                <th>Jenis Treatment</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($orders as $o)
+                            <tr>
+                                <th>{{$loop->iteration}}</th>
+                                <!-- <th>{{$o->id}}</th> -->
+                                <th>{{$o->created_at}}</th>
+                                <th>{{$o->berat}}</th>
+                                <th>{{$o->total}}</th>
+                                <th>{{$o->deskripsi}}</th>
+                                <th>{{$o->t_name}}</th>
+                                <th>{{$o->status}}</th>
+                            </tr>
+                            @endforeach
+                            <!-- Masukin syntax sql disini -->
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
